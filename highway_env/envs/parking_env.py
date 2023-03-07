9b325de70ff4a2d0cbe8095c7cad97abbe0f0e0e
sowmen <sowmendipta@gmail.com>
2023-03-07 06:27:51 +0000

    fixed v0

--- highway_env/envs/parking_env.py
+++ highway_env/envs/parking_env.py
@@ -11,6 +11,7 @@ from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.graphics import VehicleGraphics
 from highway_env.vehicle.kinematics import Vehicle
 from highway_env.vehicle.objects import Landmark, Obstacle
+from highway_env.utils import are_polygons_intersecting
 
 
 class GoalEnv(Env):
