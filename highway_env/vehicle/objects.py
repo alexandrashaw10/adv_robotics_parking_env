a229f49e4900d19b18af4793840c727ab028b6d6
sowmen <sowmendipta@gmail.com>
2023-03-07 06:26:05 +0000

    fixed parking v0 error

--- highway_env/vehicle/objects.py
+++ highway_env/vehicle/objects.py
@@ -4,9 +4,9 @@ import numpy as np
 
 from highway_env import utils
 
-if TYPE_CHECKING:
-    from highway_env.road.lane import AbstractLane
-    from highway_env.road.road import Road
+
+from highway_env.road.lane import AbstractLane
+from highway_env.road.road import Road
 
 LaneIndex = Tuple[str, str, int]
 
